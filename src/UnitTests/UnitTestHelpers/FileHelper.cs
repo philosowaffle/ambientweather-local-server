d6ce2957eba49a0abe5fb25bbb4479cda9065ce2
Bailey Belvis <baileyb622@gmail.com>
2022-10-20 18:02:24 -0500

    add pr checks and a couple tests

---
+++ src/UnitTests/UnitTestHelpers/FileHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTests.UnitTestHelpers;
+public static class FileHelper
+{
+	public static readonly string DataDirectory = Path.Join(Directory.GetCurrentDirectory(), "..", "..", "..", "Data");
+
+	public static async Task<string> ReadTextFromFileAsync(string path)
+	{
+		using (var reader = new StreamReader(path))
+		{
+			var content = await reader.ReadToEndAsync();
+			return content;
+		}
+	}
+}
