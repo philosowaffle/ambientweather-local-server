fd1a352e5ab9d37720fcc7e80ea1977886a89dfd
Bailey Belvis <baileyb622@gmail.com>
2022-11-12 17:06:22 -0600

    add pyroscope

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,9 @@ chmod 770 -R /app
 
 if [[ "$1" == "api" ]]; then
     exec runuser -u awuser ./api/Api
+elif [["$1" == "pyroscope" && "$2" == "api"]] then
+    echo Launching with Pyroscope
+    pyroscope exec runuser -u awuser ./api/Api
 else
    echo "Nothing to do."
 fi
