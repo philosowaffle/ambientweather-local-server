e9a06293608b61b685a49b5d6c340861ea4b8f2e
Bailey <baileyb622@gmail.com>
2022-11-13 07:43:26 -0600

    disable server gc

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,7 +8,7 @@ if [[ "$1" == "api" ]]; then
     exec runuser -u awuser ./api/Api
 elif [[ "$1" == "pyroscope" ]]; then
     echo Launching with Pyroscope
-    exec pyroscope exec -spy-name dotnetspy ./api/Api
+    exec pyroscope exec -spy-name dotnetspy -user-name awuser -group-name awuser ./api/Api
 else
    echo "Nothing to do."
 fi
